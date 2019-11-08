8a9e9b0f9feecfb329ca0ea9adbe8b7dc023ad41
Rahul Kaliyath <31266923+k30rahul@users.noreply.github.com>
2019-11-08 18:37:44 +0530

    Problem Statement
    Using Regex, we can easily match or search for patterns in a text. Before searching for a pattern, we have to specify one using some well-defined syntax.
    
    In this problem, you are given a pattern. You have to check whether the syntax of the given pattern is valid.
    
    Note: In this problem, a regex is only valid if you can compile it using the Pattern.compile method.
    
    Input Format
    
    The first line of input contains an integer , denoting the number of test cases. The next  lines contain a string of any printable characters representing the pattern of a regex.
    
    Output Format
    
    For each test case, print Valid if the syntax of the given pattern is correct. Otherwise, print Invalid. Do not print the quotes.
    
    Sample Input
    
    3
    ([A-Z])(.+)
    [AZ[a-z](a-z)
    batcatpat(nat
    Sample Output
    
    Valid
    Invalid
    Invalid

--- Strings/PatternSyntaxChecker.java
+++ Strings/PatternSyntaxChecker.java
@@ -6,9 +6,16 @@ public class Solution
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
-		while(testCases>0){
+		while(testCases-->0){
 			String pattern = in.nextLine();
-          	//Write your code
+            try{
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            }
+            catch(Exception e){
+                System.out.println("Invalid");
+            }
+          	
 		}
 	}
 }
